3cfc6dbbf3178fc85141309d6fdcb256ab1a79a4
Kevin Mercado <kjhosepmercado@unicesar.edu.co>
2026-08-19 08:21:19 -0500

    feat: create Material class as parent of Book

---
+++ src/main/java/com/mycompany/biblioteca/Material.java
@@ -0,0 +1,29 @@
+
+package com.mycompany.biblioteca;
+
+public class Material {
+    protected String code;
+    protected String title;
+    protected String publicationYear;
+
+    public Material(String code, String title, String publicationYear) {
+        this.code = code;
+        this.title = title;
+        this.publicationYear = publicationYear;
+    }
+
+    public String getCode() { return code; }
+    public void setCode(String code) { this.code = code; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getPublicationYear() { return publicationYear; }
+    public void setPublicationYear(String publicationYear) { this.publicationYear = publicationYear; }
+
+    @Override
+    public String toString() {
+        return "Code: " + code + " | Title: " + title + " | Year: " + publicationYear;
+    }
+}
+
